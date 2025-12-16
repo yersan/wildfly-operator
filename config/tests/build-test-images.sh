9673aa1ecd18c1aa2c019f73d7a5c5bddd1ed5ae
Yeray Borges <yborgess@redhat.com>
2025-12-16 14:04:35 +0000

    Update wildfly test versions to 38

--- config/tests/build-test-images.sh
+++ config/tests/build-test-images.sh
@@ -7,7 +7,7 @@ echo "WORKDIR: ${WORKDIR}"
 
 QUICKSTART_REPO_URL=https://github.com/wildfly/quickstart.git
 QUICKSTART_REPO_DIR="${WORKDIR}/wildfly-operator-quickstart"
-QUICKSTART_BRANCH=${1:-"35.0.1.Final"}
+QUICKSTART_BRANCH=${1:-"38.0.1.Final"}
 QUICKSTART=helloworld
 
 BOOTABLE_JAR_REPO_URL=https://github.com/wildfly-extras/wildfly-jar-maven-plugin.git
@@ -17,7 +17,7 @@ BOOTABLE_JAR=examples/microprofile-config
 
 CLUSTER_BENCH_REPO_URL=https://github.com/clusterbench/clusterbench.git
 CLUSTER_BENCH_REPO_DIR="${WORKDIR}/clusterbench"
-CLUSTER_BENCH_BRANCH=${3:-"12.0.1.Final"}
+CLUSTER_BENCH_BRANCH=${3:-"12.0.0.Final"}
 CLUSTER_BENCH=clusterbench-ee10-ear
 
 echo "Cloning quickstart repo"
@@ -26,7 +26,7 @@ git clone ${QUICKSTART_REPO_URL} "${QUICKSTART_REPO_DIR}"
 cd "${QUICKSTART_REPO_DIR}" && git checkout ${QUICKSTART_BRANCH}
 
 echo "Building quickstart image"
-cd "${QUICKSTART_REPO_DIR}"/${QUICKSTART} && mvn -Popenshift package wildfly:image \
+cd "${QUICKSTART_REPO_DIR}"/${QUICKSTART} && mvn -U -Popenshift package wildfly:image \
 -Dwildfly.image.name=wildfly/wildfly-test-image \
 -Dwildfly.image.tag=0.0
 
@@ -37,7 +37,7 @@ cd "${BOOTABLE_JAR_REPO_DIR}" && git checkout ${BOOTABLE_JAR_BRANCH}
 
 echo "Building Bootable JAR repo"
 cd "${BOOTABLE_JAR_REPO_DIR}" && git checkout ${BOOTABLE_JAR_BRANCH}
-cd "${BOOTABLE_JAR_REPO_DIR}"/${BOOTABLE_JAR} && mvn -Popenshift package
+cd "${BOOTABLE_JAR_REPO_DIR}"/${BOOTABLE_JAR} && mvn -U -Popenshift package
 
 echo "Building bootable JAR image"
 cp "${BOOTABLE_JAR_REPO_DIR}/${BOOTABLE_JAR}/target/microprofile-config-bootable.jar" "${WORKDIR}"
@@ -59,6 +59,6 @@ cd "${CLUSTER_BENCH_REPO_DIR}" && git checkout ${CLUSTER_BENCH_BRANCH}
 
 echo "Building Clusterbench repo"
 cd "${CLUSTER_BENCH_REPO_DIR}" && git checkout ${CLUSTER_BENCH_BRANCH}
-cd "${CLUSTER_BENCH_REPO_DIR}/${CLUSTER_BENCH}" && mvn -Popenshift package wildfly:image \
+cd "${CLUSTER_BENCH_REPO_DIR}/${CLUSTER_BENCH}" && mvn -U -Popenshift package wildfly:image \
 -Dwildfly.image.name=wildfly/clusterbench-test-image \
 -Dwildfly.image.tag=0.0
